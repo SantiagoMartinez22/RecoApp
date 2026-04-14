df5303b1dd5e276264adf4b2bb097b0df1b4784c
miguel <miguel.aristizabalp.col@upb.edu.co>
2026-04-14 18:50:38 -0500

    feat(tmdb): add detail DTOs and API endpoints for movie/tv

--- app/src/main/java/com/reco/app/data/remote/TmdbApi.kt
+++ app/src/main/java/com/reco/app/data/remote/TmdbApi.kt
@@ -1,6 +1,7 @@
 package com.reco.app.data.remote
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TmdbApi {
@@ -17,4 +18,18 @@ interface TmdbApi {
         @Query("query") query: String,
         @Query("language") language: String = "es-ES",
     ): SearchResponse
+
+    @GET("movie/{id}")
+    suspend fun movieDetail(
+        @Path("id") id: Int,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "es-ES",
+    ): MovieDetailDto
+
+    @GET("tv/{id}")
+    suspend fun tvDetail(
+        @Path("id") id: Int,
+        @Query("api_key") apiKey: String,
+        @Query("language") language: String = "es-ES",
+    ): TvDetailDto
 }
--- app/src/main/java/com/reco/app/data/remote/TmdbDto.kt
+++ app/src/main/java/com/reco/app/data/remote/TmdbDto.kt
@@ -23,3 +23,32 @@ data class TrendingItemDto(
     @SerializedName("first_air_date") val firstAirDate: String?,
     @SerializedName("genre_ids") val genreIds: List<Int>?,
 )
+
+data class GenreDto(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+)
+
+data class MovieDetailDto(
+    @SerializedName("id") val id: Int,
+    @SerializedName("title") val title: String?,
+    @SerializedName("overview") val overview: String?,
+    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("backdrop_path") val backdropPath: String?,
+    @SerializedName("vote_average") val voteAverage: Double?,
+    @SerializedName("release_date") val releaseDate: String?,
+    @SerializedName("runtime") val runtime: Int?,
+    @SerializedName("genres") val genres: List<GenreDto>?,
+)
+
+data class TvDetailDto(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String?,
+    @SerializedName("overview") val overview: String?,
+    @SerializedName("poster_path") val posterPath: String?,
+    @SerializedName("backdrop_path") val backdropPath: String?,
+    @SerializedName("vote_average") val voteAverage: Double?,
+    @SerializedName("first_air_date") val firstAirDate: String?,
+    @SerializedName("episode_run_time") val episodeRunTime: List<Int>?,
+    @SerializedName("genres") val genres: List<GenreDto>?,
+)
